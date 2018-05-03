7dcb4361a1b499f05736b73cb66f86214434f956
soryrawyer <sory.rawyer@gmail.com>
2018-05-03 14:57:43 -0400

    connected the arduino, but I don't think it's reading fast enough

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,6 @@ fn main() {
     let mut sample_clock = 0f32;
     let mut frequency: f32 = 440.0;
     let mut port_reader = PortReader::new("/dev/cu.usbmodem1411");
-    // let mut amplitude: f32 = 0.0;
-    // let mut frequency_range = RandRange::new(440.0, 220.0, 880.0, 0.005);
-    // let mut amplitude_range = RandRange::new(1.0, 0.5, 7.0, 0.005);
-
-    // let mut port = serialport::open("/dev/cu.usbmodem1411").unwrap();
-    // let mut serial_buf: Vec<u8> = vec![0; 1];
 
     println!("{:?}", sample_rate);
     println!("{:?}", sample_clock);
@@ -27,21 +21,6 @@ fn main() {
     let mut next_value = || {
         sample_clock = (sample_clock + 1.0 ) % sample_rate;
         // frequency = (port_reader.read_value() + 300) as f32;
-        // println!("{:?}", frequency);
-        // amplitude = amplitude_range.next();
-        // println!("frequency: {}", frequency);
-        // println!("amplitude: {}", amplitude);
-        // A * sin(2 * pi * frequency * (sample clock / sample rate (44.1khz)))
-        // amplitude * (sample_clock * frequency * 2.0 * 3.141592 / sample_rate).sin()
-        // match port.read(serial_buf.as_mut_slice()) {
-        //     // Ok(t) => io::stdout().write_all(&serial_buf[..t]).unwrap(),
-        //     Ok(_) => {
-        //         frequency = serial_buf[0] as f32;
-        //         println!("frequency: {}", frequency);
-        //     },
-        //     Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-        //     Err(e) => println!("got this error: {:?}", e),
-        // };
         (2.0 * (sample_clock * frequency * 2.0 * 3.141592 / sample_rate).sin())
     };
 
@@ -76,34 +55,6 @@ fn main() {
     });
 }
 
-// RandRange will take maximum and minimum values and a step, then fluctuate
-struct RandRange {
-    curr: f32,
-    next: f32,
-    step: f32,
-    direction: i8,
-    max: f32,
-    min: f32,
-}
-
-impl RandRange {
-    fn next(&mut self) -> f32 {
-        let mut new_next = self.next + (self.step * self.direction as f32);
-        if new_next > self.max || new_next < self.min {
-            self.direction *= -1;
-            new_next = self.next + (self.step * self.direction as f32);
-        }
-        self.curr = self.next;
-        self.next = new_next;
-        self.curr
-    }
-
-    fn new(curr: f32, min: f32, max: f32, step: f32) -> RandRange {
-        let next = curr + step;
-        let direction = 1;
-        RandRange{curr, next, step, direction, max, min}
-    }
-}
 
 struct PortReader {
     port: Box<serialport::SerialPort>,
