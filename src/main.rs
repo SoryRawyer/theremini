7a7e7c82dcde21a69d08e31b22faffe6b1b1359d
soryrawyer <sory.rawyer@gmail.com>
2018-05-03 17:29:42 -0400

    it almost works!

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ extern crate serialport;
 
 use std::io;
 
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
     let device = cpal::default_output_device().expect("failed to get output device");
     let format = device.default_output_format().expect("failed to get output format");
@@ -14,13 +18,37 @@ fn main() {
     let mut sample_clock = 0f32;
     let mut frequency: f32 = 440.0;
     let mut port_reader = PortReader::new("/dev/cu.usbmodem1411");
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+    let mut i = 0;
+    let mut j = 0;
+
+    thread::spawn(move || {
+        loop {
+            // let mut now = Instant::now();
+            j += 1;
+            if j % 1000 == 0 {
+                tx.send(port_reader.read_value()).unwrap();
+            }
+            // times.push(now.elapsed().subsec_nanos());
+        }
+    });
 
     println!("{:?}", sample_rate);
     println!("{:?}", sample_clock);
 
-    let mut next_value = || {
-        sample_clock = (sample_clock + 1.0 ) % sample_rate;
+    let mut next_value = move || {
+        // let now = Instant::now();
+        i += 1;
+        if i % 1000 == 0 {
+            if let Ok(val) = rx.recv() {
+                // println!("{:?}", val);
+                frequency = (val + 100) as f32; 
+            }
+        }
+
         // frequency = (port_reader.read_value() + 300) as f32;
+        sample_clock = (sample_clock + 1.0 ) % sample_rate;
+        // println!("{:?}", now.elapsed());
         (2.0 * (sample_clock * frequency * 2.0 * 3.141592 / sample_rate).sin())
     };
 
@@ -86,7 +114,11 @@ impl PortReader {
                         if serial_buf[i] == 10 {
                             // println!("{:?}", value);
                             self.unread_stuff = serial_buf[(i+1)..t].to_vec();
-                            return String::from_utf8(value).unwrap().parse().unwrap()
+                            if let Ok(val) = String::from_utf8(value).unwrap().parse() {
+                                return val
+                            } else {
+                                return 0
+                            }
                         } else {
                             value.extend(vec!(serial_buf[i]));
                         }
